0f9f26695b2affd4589c2c956b223c09882f9ec1
Gator <allidodt@gmail.com>
2022-11-04 18:38:10 -0700

    sql, inquirer, console.table

---
+++ server.js
@@ -0,0 +1,6 @@
+//import mysql, inquirer, dotenv, console.table
+const mysql = require ('mysql12');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+require('dotenv').config();
